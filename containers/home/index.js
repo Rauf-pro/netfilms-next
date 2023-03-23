ffd1e4429adf9301f3120ba971418d80f4891658
rrauf8178 <rrauf8178@gmail.com>
2023-03-23 17:25:15 +0400

    Added HomeContainer

--- containers/home/index.js
+++ containers/home/index.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import FeaturedMovie from '@/components/featured-movie';
+import Movies from '@/mocks/movies.json'
 
 export default function HomeContainer() {
   return (
-    <div>HomeContainer</div>
+    <div>
+        <FeaturedMovie movie={Movies.results[0]}/>
+    </div>
   )
 }
